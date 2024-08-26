39ac30c7939021745811905582835962815103b9
Diwakar Kushwaha <diwakar.nitk@gmail.com>
2024-08-26 09:45:33 +0200

    Added Response Model

--- fetch-data/src/main/java/org/ipo/fetch/model/IPOResponse.java
+++ fetch-data/src/main/java/org/ipo/fetch/model/IPOResponse.java
@@ -3,5 +3,15 @@ package org.ipo.fetch.model;
 public class IPOResponse {
     private String ipoId;
     private String status;
-    private String name;
+    private String ipoName;
+    private int listingPrice;
+    private int gmp;
+    private int estListing;
+    private double ipoSize;
+    private int lot;
+    private String openDt;
+    private String closeDt;
+    private String boaDate;
+    private String listingDate;
+
 }
